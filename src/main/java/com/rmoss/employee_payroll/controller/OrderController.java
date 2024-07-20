9e8f61d9cfe793484337028199ef8ac2b6bc01db
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-07-21 00:41:56 +0200

    feat: Add order processing functionality

--- src/main/java/com/rmoss/employee_payroll/controller/OrderController.java
+++ src/main/java/com/rmoss/employee_payroll/controller/OrderController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -85,5 +86,24 @@ public class OrderController {
                 .body(Problem.create() //
                         .withTitle("Method not allowed") //
                         .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+    }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+
+        Order order = orderRepository.findById(id) //
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity //
+                .status(HttpStatus.METHOD_NOT_ALLOWED) //
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+                .body(Problem.create() //
+                        .withTitle("Method not allowed") //
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
     }
 }
